d1af5dc9c9385099dba0639a9f90fefc7d7d73f5
Ran Kim <rannio@gmail.com>
2022-04-05 21:02:28 -0400

    added more to details

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { LoadMovieDetails } from '../store/actions/MovieActions'
 
@@ -21,10 +22,18 @@ const MovieDetails = (props) => {
   },[id])
 console.log(props.movieDetailsState.movieDetails)
   return (
-    <ul>
-        {props.movieDetailsState.movieDetails.overview}
-
-    </ul>
+      <div>
+        <ul>
+            <h1>{props.movieDetailsState.movieDetails.original_title}</h1>
+            <h3>Overview</h3>
+            <p>{props.movieDetailsState.movieDetails.overview}</p>
+            <h3>Release Date</h3>
+            <p>{props.movieDetailsState.movieDetails.release_date}</p>
+            <h3>Runtime</h3>
+            <p>{props.movieDetailsState.movieDetails.runtime}</p>
+        </ul>
+        <Link to={'/'}>Back</Link>
+    </div>
   )
 }
 
